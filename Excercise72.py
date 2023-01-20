24a171464b160abd17e9823e3a5fad1f18bd349f
Mr. Zi <md.ziaulhasan00@gmail.com>
2023-01-20 16:17:40 +0600

    7.2 done

--- Excercise72.py
+++ Excercise72.py
@@ -15,7 +15,7 @@ Write a function called eval_loop that iteratively prompts the user, takes the
 resulting input and evaluates it using eval, and prints the result.
 It should continue until the user enters 'done', and then return the value of
 the last expression it evaluated.
-----------------------------------------------------------------------------------"""
+---------------------------------------------------------------------------------"""
 import math
 def eval_loop():
     while True:
